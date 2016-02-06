67baea4c738d32f6cf831d9fbafaba4684ddfc67
Koichi Tanaka <tanaka51.jp@gmail.com>
2016-02-06 13:03:06 +0900

    rename

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_publish-my-body-weight_session'
+Rails.application.config.session_store :cookie_store, key: '_publish-my-weight_session'
